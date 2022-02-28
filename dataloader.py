cd06f4e34e67cd7d56d254b11b9a689b22b8dd6d
Aditya Rastogi <adityarastogi71@gmail.com>
2022-02-28 23:27:01 +0530

    hullo

--- dataloader.py
+++ dataloader.py
@@ -35,7 +35,7 @@ class LoadData(Dataset):
         inputImage = inputImage.transpose((2, 0, 1))
         targetImage = np.expand_dims(targetImage,axis=0)
 
-        return inputImage, targetImage,weights
+        return inputImage, targetImage,weights, self.frame.iloc[idx, 0].split("/")[-1]
 
 if __name__ == "__main__":
     rootDir ="./CoSkel+"
