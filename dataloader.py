0a0e1cda3a9f8703e42256a5509eb63a8da6493b
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-03-13 16:17:27 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -35,7 +35,7 @@ class LoadData(Dataset):
         inputImage = inputImage.transpose((2, 0, 1))
         targetImage = np.expand_dims(targetImage,axis=0)
 
-        return inputImage, targetImage,weights, self.frame.iloc[idx, 0]
+        return inputImage, targetImage,weights
 
 if __name__ == "__main__":
     rootDir ="./CoSkel+"
