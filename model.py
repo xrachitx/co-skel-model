6a7f0bdc048c7ef5f6e3211c88593e9d9d46808d
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-06-11 14:32:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -46,7 +46,9 @@ class Model(nn.Module):
         # print(self.vgg16s)
         # for vgg in self.vgg16s:
             
-        self.transpose_convs = nn.ModuleList([nn.Sequential(nn.ConvTranspose2d(512,512,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(512,256,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(256,128,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(128,64,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(64,1,2,2))])
+        self.transpose_convs = nn.ModuleList([nn.Sequential(nn.ConvTranspose2d(512,512,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(512,256,2,2),nn.ReLU()),
+                                              nn.Sequential(nn.ConvTranspose2d(256,128,2,2),nn.ReLU()),nn.Sequential(nn.ConvTranspose2d(128,64,2,2),nn.ReLU()),
+                                              nn.Sequential(nn.ConvTranspose2d(64,2,2,2))])
         self.sigmoid = nn.Sigmoid()
         self.device = device
     
