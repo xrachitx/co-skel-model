3d9d46f523dddb566483847f640860a51c2ed956
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-06-11 13:45:47 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -11,7 +11,7 @@ import cv2
 ANIMOLS = {"Aeroplane": 0, "Bear":1, "Bird":2, "Boat":3, "Bus":4, "Car":5, "Cats":6, "Cow":7, "Cycle":8, "Dog":9, "Elephant": 10, "Giraffe":11, "Horse":12, "Person":13, "Sheep":14, "Zebra":15}
 
 class LoadData(Dataset):
-    def __init__(self, fileNames, rootDir, transform=None):
+    def __init__(self, fileNames, rootDir, dice_loss,transform=None):
         self.rootDir = rootDir
         self.transform = transform
         self.frame = pd.read_csv(fileNames, dtype=str, delimiter=',', header=None)
@@ -31,6 +31,12 @@ class LoadData(Dataset):
     
         
         targetImage = targetImage > 0.0
+        if dice_loss:
+            out_im = np.zeros((448,448,2))
+            out_im[:,:,0] = np.where(targetImage == 0, 1, 0)
+            out_im[:,:,1] = np.where(targetImage == 1, 1, 0)
+            out_im = out_im.astype(np.float32)
+            out_im = out_im.transpose((2, 0, 1))
         counts = np.unique(targetImage,return_counts=True)[1]
         weights = np.array([ counts[0]/(counts[0]+counts[1]) , counts[1]/(counts[0]+counts[1]) ])
         inputImage = inputImage.astype(np.float32)
